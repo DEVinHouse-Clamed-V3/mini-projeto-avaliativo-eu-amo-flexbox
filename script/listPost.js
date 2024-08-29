8cc3fa1149fda591d302420f333c2fef0a725ec0
Lucas da Silva <sklucassilva@gmail.com>
2024-08-29 20:17:12 -0300

    feat: Update autoplay delay in swiper configuration

--- script/listPost.js
+++ script/listPost.js
@@ -2,7 +2,7 @@ var swiper = new Swiper('.mySwiper', {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 2500,
+    delay: 4500,
     disableOnInteraction: false,
   },
   pagination: {
